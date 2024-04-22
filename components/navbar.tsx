b88c7d235bb842e0ea64701f01f3a3c2fecff1e3
marlon <marlonsuyuc@gmail.com>
2024-04-22 11:49:26 -0600

    fix names files

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,6 @@ import { UserButton, auth } from '@clerk/nextjs'
 import { MainNav } from './main-nav'
 import { redirect } from 'next/navigation'
 import prismadb from '@/lib/prismadb'
-import { ThemeToggle } from './Theme-toggle'
 import StoreSwitcher from './store-switcher'
 
 export const Navbar = async () => {
@@ -25,7 +24,7 @@ export const Navbar = async () => {
         <StoreSwitcher items={stores} /> {/* Options */}
         <MainNav className='mx-6' /> {/* Links */}
         <div className='ml-auto flex items-center space-x-4'>
-          <ThemeToggle />
+          {/* <ThemeToggle /> */}
           <UserButton afterSignOutUrl='/' />
         </div>
       </div>
