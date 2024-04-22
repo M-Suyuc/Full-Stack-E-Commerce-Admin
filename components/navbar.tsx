0876381c4b70d2f7ab4e87a693acd3d983935e38
marlon <marlonsuyuc@gmail.com>
2024-04-22 11:59:24 -0600

    change files of directory

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,8 @@ import { UserButton, auth } from '@clerk/nextjs'
 import { MainNav } from './main-nav'
 import { redirect } from 'next/navigation'
 import prismadb from '@/lib/prismadb'
+import { ThemeToggle } from './theme-toggle'
+import StoreSwitcher from './store-switcher'
 
 export const Navbar = async () => {
   const { userId } = auth()
@@ -20,10 +22,10 @@ export const Navbar = async () => {
   return (
     <div className='border-b'>
       <div className='flex h-16 items-center px-4'>
-        {/* <StoreSwitcher items={stores} /> Options */}
+        <StoreSwitcher items={stores} />
         <MainNav className='mx-6' /> {/* Links */}
         <div className='ml-auto flex items-center space-x-4'>
-          {/* <ThemeToggle /> */}
+          <ThemeToggle />
           <UserButton afterSignOutUrl='/' />
         </div>
       </div>
