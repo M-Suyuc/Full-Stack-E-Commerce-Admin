cdc30f23698085f601e8ffb5d87e12fbb1a02ccd
marlon <marlonsuyuc@gmail.com>
2024-05-01 17:19:22 -0600

    add db for tursoDB

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`
+})
 
-const prismadb = globalThis.prisma || new PrismaClient()
+const adapter = new PrismaLibSQL(libsql)
+const prisma = new PrismaClient({ adapter })
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prismadb
-
-export default prismadb
+export default prisma
